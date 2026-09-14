979dcc45261388694250952d343624a5c7b1836b
piliavskaD <pilavskadasa@gmail.com>
2026-09-14 13:55:02 +0300

    add file Vorona_b

---
+++ Vorona_b.cpp
@@ -0,0 +1,9 @@
+#include "shared_types.h"
+
+Result processStudentB(const InputData& data) {
+    std::cout << "[Student B] Processing data...\n";
+    Result res;
+    res.calculatedValue = 0.0;
+    res.isSuccess = true;
+    return res;
+}
