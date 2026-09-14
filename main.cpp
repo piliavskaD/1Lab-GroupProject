b1fbcdc5b08db45aa10cca016d214d206762ba9e
piliavskaD <pilavskadasa@gmail.com>
2026-09-14 16:57:52 +0300

    add base project file

--- main.cpp
+++ main.cpp
@@ -1,18 +1,33 @@
-#include <iostream>
 #include "shared_types.h"
+#include <iostream>
+
 using namespace std;
 
-int main() {
-    cout << "--- Lab 1 Group Project ---\n";
+int main()
+{
+    auto data = make_shared<const InputData>(
+        InputData{
+            {
+                { 1.0, 2.0 },
+                { 2.0, 4.0 },
+                { 3.0, 6.0 },
+                { 4.0, 8.0 }
+            },
+            2.5
+        }
+    );
 
-    InputData input;
-    input.parameter = 10;
+    auto resultA = calculateA(data);
+    auto resultB = calculateB(data);
 
-    Result resA = processStudentA(input);
-    Result resB = processStudentB(input);
+    auto [valueA, errorA] = *resultA;
+    auto [valueB, errorB] = *resultB;
 
-    cout << "Student A(Piliavska) execution success: " << std::boolalpha << resA.isSuccess << "\n";
-    cout << "Student B(Vorona) execution success: " << std::boolalpha << resB.isSuccess << "\n";
+    cout << "Student A: value = " << valueA
+        << ", error = " << errorA << endl;
+
+    cout << "Student B: value = " << valueB
+        << ", error = " << errorB << endl;
 
     return 0;
 }
