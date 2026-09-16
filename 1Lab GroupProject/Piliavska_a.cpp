d6e2e9f73c772c243c569ea6071e689d548204e7
piliavskaD <pilavskadasa@gmail.com>
2026-09-16 18:50:05 +0300

    Add to repository

---
+++ 1Lab GroupProject/Piliavska_a.cpp
@@ -0,0 +1,7 @@
+#include "shared_types.h"
+
+unique_ptr<Result> calculateA(shared_ptr<const InputData> data)
+{
+    (void)data;
+    return make_unique<Result>(Result{ 0.0, 0.0 });
+}
---
+++ 1Lab GroupProject/main.cpp
@@ -0,0 +1,6 @@
+#include "shared_types.h"
+
+int main()
+{
+    return 0;
+}
